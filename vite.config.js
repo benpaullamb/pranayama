c670586bf28a7351b643a967d9d054995ab80936
Ben Lamb <benpaullamb@gmail.com>
2025-10-23 19:53:35 +0100

    feat: pwa settings

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,31 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: "Pranayama",
+        short_name: "Pranayama",
+        description: "4/7/8 breathing timer",
+        theme_color: "#3674b5",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
   resolve: {
